0aaa9e07bee964227092d5443d740df4238a0a59
XuBo <463467479@qq.com>
2017-07-17 14:06:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ import _MapTypes from './js/MapTypes';
 import _MapView from './js/MapView';
 import _MapModule from './js/MapModule';
 import _Geolocation from './js/Geolocation';
+import _Trace from './js/Trace';
 
 export const MapTypes = _MapTypes;
 export const MapView = _MapView;
 export const MapModule = _MapModule;
 export const Geolocation = _Geolocation;
+export const Trace = _Trace;
