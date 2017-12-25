eb51ecb7a099f5392b3acfb9c3c7a6006f2b87b0
DuYiQing <497654480@qq.com>
2017-12-25 13:50:08 +0800

    Ios端鹰眼轨迹

--- js/Trace.js
+++ js/Trace.js
@@ -14,29 +14,42 @@ import React, {
 const _module = NativeModules.BaiduTraceModule;
 
 export default {
-  beginTrace(serviceId, entityName, gatherInterval, packInterval) {
+  beginTrace(serviceId, entityName, gatherInterval, packInterval, keepAlive) {
     if (Platform.OS == 'ios') {
-
-    }
-    return new Promise((resolve, reject) => {
-      try {
-        _module.beginTrace(serviceId, entityName, gatherInterval, packInterval);
-      }
-      catch (e) {
-        reject(e);
-        return;
-      }
-      DeviceEventEmitter.once('onBeginTrace', resp => {
-        resolve(resp);
+      return new Promise((resolve, reject) => {
+        try {
+          // AK mcode entityName serviceID keepAlive
+          _module.beginTraceWithAK(serviceId, entityName, gatherInterval, packInterval, keepAlive);
+        }
+        catch (e) {
+          reject(e);
+          return;
+        }
+        DeviceEventEmitter.once('onBeginTrace', resp => {
+          resolve(resp);
+        });
       });
-    });
+    } else {
+      return new Promise((resolve, reject) => {
+        try {
+          _module.beginTrace(serviceId, entityName, gatherInterval, packInterval);
+        }
+        catch (e) {
+          reject(e);
+          return;
+        }
+        DeviceEventEmitter.once('onBeginTrace', resp => {
+          resolve(resp);
+        });
+      });
+    }
 
   },
 
   stopTrace() {
-    if (Platform.OS == 'ios') {
+    // if (Platform.OS == 'ios') {
 
-    }
+    // }
     return new Promise((resolve, reject) => {
       try {
         _module.stopTrace();
